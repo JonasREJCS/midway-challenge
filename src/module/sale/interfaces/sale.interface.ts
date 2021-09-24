d1785c9de3d75e1a501a91e8a0db12287590b77a
jonas <jj_jcs@hotmail.com>
2021-09-23 22:40:38 -0300

    update interface

--- src/module/sale/interfaces/sale.interface.ts
+++ src/module/sale/interfaces/sale.interface.ts
@@ -2,6 +2,7 @@ import { FiscalNoteInterface } from ".";
 import { ProductInterface } from "../../product/product.interface";
 
 export interface SaleInterface {
+    _id?: string,
     productData: Pick<ProductInterface, 'id' | 'nome' | 'tamanho' | 'tipo' | 'descricao'>,
     fiscalNote: FiscalNoteInterface
 }
