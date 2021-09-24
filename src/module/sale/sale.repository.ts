b6338a5dd6ed79b980422a21b05c4d1c5b6c21a5
jonas <jj_jcs@hotmail.com>
2021-09-23 22:40:30 -0300

    removed unused function and change save method

--- src/module/sale/sale.repository.ts
+++ src/module/sale/sale.repository.ts
@@ -7,12 +7,7 @@ import { SaleInterface } from "./interfaces"
 export class SaleRepository {
     constructor(@InjectModel('sale') private saleModel: Model<SaleInterface>) { }
 
-    public async findById(id: number): Promise<SaleInterface> {
-        const result: SaleInterface = await this.saleModel.findOne({ id: id })
-        return result
-    }
-
     public async save(sale: SaleInterface): Promise<SaleInterface> {
-        return await this.saleModel.findOneAndUpdate({ id: sale.fiscalNote.id }, sale, { upsert: true })
+        return await this.saleModel.create(sale)
     }
 }
