3f275d40d44bbf265d7096f83d2768d2e254b1d5
jonas <jj_jcs@hotmail.com>
2021-09-24 00:19:22 -0300

    add delete method

--- src/module/sale/sale.repository.ts
+++ src/module/sale/sale.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common"
 import { Model } from "mongoose"
 import { InjectModel } from "@nestjs/mongoose"
 import { SaleInterface } from "./interfaces"
+import { CancelSaleDTO } from "./dtos"
 
 @Injectable()
 export class SaleRepository {
@@ -9,5 +10,17 @@ export class SaleRepository {
 
     public async save(sale: SaleInterface): Promise<SaleInterface> {
         return await this.saleModel.create(sale)
+    }
+
+    public async delete(sale: SaleInterface): Promise<void> {
+        await this.saleModel.findByIdAndDelete(sale._id)
+    }
+
+    public async findOne(cancelSaleDTO: CancelSaleDTO): Promise<SaleInterface> {
+        return await this.saleModel.findOne(
+            {  "fiscalNote.id": cancelSaleDTO.fiscalNoteId },
+            {  "fiscalNote.cpf": cancelSaleDTO.cpf },
+            {  "fiscalNote.productId": cancelSaleDTO.productId },
+        )
     }
 }
