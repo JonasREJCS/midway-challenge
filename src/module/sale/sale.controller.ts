de0fccf9e8046c3c6aff37025daac6300b92c9d9
jonas <jj_jcs@hotmail.com>
2021-09-23 23:27:17 -0300

    added try catch

--- src/module/sale/sale.controller.ts
+++ src/module/sale/sale.controller.ts
@@ -8,14 +8,21 @@ import { SaleInterface } from './interfaces';
 import { SaleService } from './sale.service';
 
 @Controller('sale')
-export class SaleController { 
-    constructor(private saleService: SaleService) {}
+export class SaleController {
+  constructor(private saleService: SaleService) { }
 
-    @Post()
-    async registerSale(
-      @Body(ValidationPipe) registerSaleDTO: RegisterSaleDTO,
-    ): Promise<SaleInterface> {
+  @Post()
+  async registerSale(
+    @Body(ValidationPipe) registerSaleDTO: RegisterSaleDTO,
+  ): Promise<SaleInterface | any> {
+    try {
       const result = await this.saleService.registerSale(registerSaleDTO);
       return result
+    } catch (error) {
+      return {
+        error: true,
+        message: error.message
+      }
     }
+  }
 }
