edbd4993d8bd89dc66f371111dd2344028fb28d4
jonas <jj_jcs@hotmail.com>
2021-09-24 02:11:04 -0300

    added validation to ENV VARs

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,5 +1,17 @@
 require('dotenv').config()
 const { env } = process;
 
+const REQUIRED_ENV_VARS = [
+    'DB_URI'
+]
+
+export function validateEnvVars(): void {
+    REQUIRED_ENV_VARS.forEach((envVar) => {
+        const val = process.env[envVar];
+        if (val === '' || val === null || val === undefined) {
+            throw new Error(`Required ENV VAR not set: ${envVar}`);
+        }
+    });
+}
 
 export const dbURI = env.DB_URI;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { validateEnvVars } from './core/config';
 import { ProductService } from './module/product/product.service';
 
 async function bootstrap() {
+  validateEnvVars()
+  
   const app = await NestFactory.create(AppModule);
   const productService = app.get(ProductService);
   productService.initProductsDatabase()
