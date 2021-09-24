f44af0ff936003491da9a9aca3deae0ad2e9651c
jonas <jj_jcs@hotmail.com>
2021-09-24 00:19:14 -0300

    add cancellation method

--- src/module/sale/sale.service.ts
+++ src/module/sale/sale.service.ts
@@ -9,6 +9,7 @@ import { v4 } from 'uuid';
 import { SaleErrorsEnum } from './saleErrors.enum';
 import { ProductService } from '../product/product.service';
 import { SaleRepository } from './sale.repository';
+import { CancelSaleDTO } from './dtos';
 
 
 
@@ -23,12 +24,13 @@ export class SaleService {
     async registerSale(registerSaleDTO: RegisterSaleDTO): Promise<SaleInterface> {
         try {
             if (!registerSaleDTO?.soldProductId) throw new Error(SaleErrorsEnum.INVALID_PRODUCT_ID)
-            if (!registerSaleDTO?.cpf.trim()) throw new Error(SaleErrorsEnum.INVALID_CPF)
+            if (!registerSaleDTO?.cpf || !registerSaleDTO?.cpf.trim()) throw new Error(SaleErrorsEnum.INVALID_CPF)
             if (!registerSaleDTO?.saleDate) throw new Error(SaleErrorsEnum.INVALID_DATE)
 
             const soldProduct = await this.productService.getProductById(registerSaleDTO.soldProductId)
 
             if (!soldProduct) throw new Error(SaleErrorsEnum.PRODUCT_NOT_FOUND)
+            if (soldProduct.estoque < 1) throw new Error(SaleErrorsEnum.PRODUCT_OUT_OF_STOCK)
 
             const sale: SaleInterface = {
                 fiscalNote: {
@@ -45,8 +47,14 @@ export class SaleService {
                     id: soldProduct.id
                 }
             };
+            
+            soldProduct.estoque -= 1
 
-            await this.saleRepository.save(sale)
+            await Promise.all([
+                await this.saleRepository.save(sale),
+                await this.productService.save(soldProduct)
+            ])
+
 
             this.logger.log(`Registered sale [${sale.fiscalNote.id}]`)
 
@@ -57,4 +65,37 @@ export class SaleService {
         }
     }
 
+    private validateCancelDTO(cancelSaleDTO: CancelSaleDTO) {
+        if (!cancelSaleDTO?.fiscalNoteId)
+            throw new Error(SaleErrorsEnum.INVALID_PRODUCT_ID);
+        if (!cancelSaleDTO?.cpf || !cancelSaleDTO?.cpf.trim())
+            throw new Error(SaleErrorsEnum.INVALID_CPF);
+        if (!cancelSaleDTO?.productId)
+            throw new Error(SaleErrorsEnum.INVALID_PRODUCT_ID);
+    }
+
+    async cancelSale(cancelSaleDTO: CancelSaleDTO): Promise<void> {
+        try {
+            this.validateCancelDTO(cancelSaleDTO);
+
+            const soldProduct = await this.productService.getProductById(cancelSaleDTO.productId)
+            if (!soldProduct) throw new Error(SaleErrorsEnum.PRODUCT_CANCELATION_NOT_FOUND)
+
+            const foundSale: SaleInterface = await this.saleRepository.findOne(cancelSaleDTO)
+            if (!foundSale) throw new Error(SaleErrorsEnum.SALE_CANCELATION_NOT_FOUND)
+
+            soldProduct.estoque += 1
+            soldProduct.dataAtualizacao = new Date()
+
+            await Promise.all([
+                await this.saleRepository.delete(foundSale),
+                await this.productService.save(soldProduct)
+            ])
+
+            this.logger.log(`Canceled fiscal note: [${cancelSaleDTO.fiscalNoteId}]`)
+        } catch (error) {
+            this.logger.error(error.message)
+            throw error
+        }
+    }
 }
