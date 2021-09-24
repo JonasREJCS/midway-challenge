e697e4f2ec73d5eca56fbea583afab8ab4df03f6
jonas <jj_jcs@hotmail.com>
2021-09-24 02:00:18 -0300

    add item to enum

--- src/module/sale/saleErrors.enum.ts
+++ src/module/sale/saleErrors.enum.ts
@@ -3,8 +3,8 @@ export enum SaleErrorsEnum {
     INVALID_CPF = 'Sale registration error: invalid cpf',
     INVALID_DATE = 'Sale registration error: invalid date',
     INVALID_FISCAL_NOTE_ID = 'Sale registration error: invalid fiscalNoteId',
-    PRODUCT_NOT_FOUND = 'Sale registration error: product not found',
-    PRODUCT_CANCELATION_NOT_FOUND = 'Sale cancelation error: product not found',
+    REGISTRATION_PRODUCT_NOT_FOUND = 'Sale registration error: product not found',
+    CANCELATION_PRODUCT_NOT_FOUND = 'Sale cancelation error: product not found',
     SALE_CANCELATION_NOT_FOUND = 'Sale cancelation error: sale not found',
     PRODUCT_OUT_OF_STOCK = 'Sale registration error: product out of stock',
 }
